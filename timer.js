0c5fd4acef1847fcacec4d87f75af4c49e1f9c30
prabhatojha2403 <108303357+prabhatojha2403@users.noreply.github.com>
2022-09-12 23:59:21 +0530

    hi

---
+++ timer.js
@@ -0,0 +1,14 @@
+var countDownDate = new Data ("Aug 22, 2022 17:23:33").getTime();
+var x = setInterval (function() {
+    var now = new Data().getTime();
+    var distance = countDownDate - now ;
+    var days = Math.floor(distance/(1000*60*60*24));
+    var hours = Math.floor ((distance % (1000 * 60 * 60 *24 ))/ (1000 * 60 * 60));
+    var minutes = Math.floor ((distance % (1000 * 60 * 60 ))/ (1000 * 60));
+    var seconds = Math.floor ((distance % (1000 * 60))/1000);
+    document.getElementById("demo").innerHTML = days + "d" + hours + "h" + minutes + "m" + seconds + "s";
+    if (distance <0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML= "EXPIRED";
+    }
+},1000);
